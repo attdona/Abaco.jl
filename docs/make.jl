583f82fc8762b9b7bcc3a7fc6d8d007599be55c1
attdona <attilio.dona@telecomitalia.it>
2024-02-12 10:56:15 +0100

    Remove repo

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ DocMeta.setdocmeta!(Abaco, :DocTestSetup, :(using Abaco); recursive=true)
 makedocs(;
     modules=[Abaco],
     authors="Attilio <attilio.dona@telecomitalia.it> and contributors",
-    repo="https://github.com/attdona/Abaco.jl/blob/{commit}{path}#{line}",
     sitename="Abaco.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
@@ -20,5 +19,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/attdona/Abaco.jl",
-    devbranch = "main"
+    devbranch="main"
 )
